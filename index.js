616331ff5a9c79f6cdc72f987de6bb813953d172
Robert Dudley <robert.dudley@dillards.com>
2016-09-30 11:03:42 -0500

    update viewport

--- index.js
+++ index.js
@@ -5,16 +5,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-	// var result = ''
-
-  
-	
-
-	// // lifting();
-	// result = response;
-	// response.send( result );
-});
+app.get('/', function(request, response) {});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
